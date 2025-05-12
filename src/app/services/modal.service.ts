c8bf4312712078df573def1732a7af418c237eb9
Brooke <133279077+Beerooke31@users.noreply.github.com>
2025-05-12 20:34:23 +0800

    Created method to toggle modal open

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -11,4 +11,22 @@ interface IModal {
 export class ModalService {
   private modals = signal<IModal[]>([]);
   constructor() {}
+  register(id: string, element: HTMLDialogElement) {
+    this.modals.set([
+      ...this.modals(),
+      {
+        id,
+        element,
+      },
+    ]);
+  }
+  toggle(id: string) {
+    const modal = this.modals().find((item) => item.id === id);
+    if (!modal) return;
+    if (modal.element.open) {
+      modal.element.close();
+    } else {
+      modal.element.showModal();
+    }
+  }
 }
