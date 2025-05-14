a9c35a7df623ef0da8384144f7eb493da75d4a0b
Brooke <133279077+Beerooke31@users.noreply.github.com>
2025-05-14 20:30:01 +0800

    Got tabs for login and registration toggling correctly

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
   imports: [],
   templateUrl: './tab.component.html',
-  styleUrl: './tab.component.css'
+  styleUrl: './tab.component.css',
 })
 export class TabComponent {
-
+  tabTitle = input('');
+  active = signal(false);
 }
--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, contentChildren, AfterContentInit } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './tabs-container.component.html',
-  styleUrl: './tabs-container.component.css'
+  styleUrl: './tabs-container.component.css',
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
+  tabs = contentChildren(TabComponent);
 
+  ngAfterContentInit() {
+    const activeTab = this.tabs().find((tab) => tab.active());
+
+    if (!activeTab) {
+      this.selectTab(this.tabs()[0]);
+    }
+  }
+  selectTab(tab: TabComponent) {
+    this.tabs().forEach((tab) => tab.active.set(false));
+
+    tab.active.set(true);
+
+    return false;
+  }
 }
