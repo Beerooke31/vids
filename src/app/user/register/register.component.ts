dfc9544e8afbf5911bb72e8cc47770396b6e8ada
Brooke <133279077+Beerooke31@users.noreply.github.com>
2025-05-19 20:32:56 +0800

    Added form controls and error message for Name field

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  fb = inject(FormBuilder);
 
+  form = this.fb.nonNullable.group({
+    name: ['', [Validators.required]],
+    age: [18],
+    email: [''],
+    password: [''],
+    confirmPassword: [''],
+    phoneNumber: [''],
+  });
 }
