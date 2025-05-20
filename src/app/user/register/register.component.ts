d669a5d04975044857247224c1c137ca6197f8db
Brooke <133279077+Beerooke31@users.noreply.github.com>
2025-05-20 20:37:24 +0800

    Added component for form validation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { InputComponent } from '../../shared/input/input.component';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, InputComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
@@ -12,7 +13,7 @@ export class RegisterComponent {
   fb = inject(FormBuilder);
 
   form = this.fb.nonNullable.group({
-    name: ['', [Validators.required]],
+    name: ['', [Validators.required, Validators.minLength(3)]],
     age: [18],
     email: [''],
     password: [''],
