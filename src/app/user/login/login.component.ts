feb3c3679b8e2e0a6534c9cd37ef66800e01d27a
Brooke <133279077+Beerooke31@users.noreply.github.com>
2025-05-29 06:50:45 +0800

    Added alert for login success and error

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
 import { AlertComponent } from '../../shared/alert/alert.component';
@@ -6,7 +6,7 @@ import { AlertComponent } from '../../shared/alert/alert.component';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, AlertComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -18,16 +18,34 @@ export class LoginComponent {
   };
 
   submitted = false;
+  showAlert = signal(false);
+  alertMsg = signal('Hold please. Logging you in.');
+  alertColour = signal('blue');
+  inSubmission = signal(false);
 
   async login() {
+    this.showAlert.set(true);
+    this.alertMsg.set('Hold please. Logging you in.');
+    this.alertColour.set('blue');
+    this.inSubmission.set(true);
+
     try {
-      signInWithEmailAndPassword(
+      await signInWithEmailAndPassword(
         this.auth,
         this.credentials.email,
         this.credentials.password
       );
     } catch (err) {
       console.error(err);
+      this.alertMsg.set(
+        'An unexpected error occurred! Please try again later.'
+      );
+      this.alertColour.set('red');
+      this.inSubmission.set(false);
+      return;
     }
+
+    this.alertMsg.set("Success! You've been logged in.");
+    this.alertColour.set('green');
   }
 }
