6c7fa36b73d74bbf362f8a663f38270568906169
Brooke <133279077+Beerooke31@users.noreply.github.com>
2025-06-04 06:41:45 +0800

    Updated validation message for mismatched passwords

--- src/app/user/validators.ts
+++ src/app/user/validators.ts
@@ -8,10 +8,14 @@ export function Match(
     const control = group.get(controlName);
     const matchingControl = group.get(matchingControlName);
 
-    if (!control || !matchingControl) return { controlNotFound: false };
-
+    if (!control || !matchingControl) {
+      console.error('Form controls cannot be found in the form group.');
+      return { controlNotFound: false };
+    }
     const error =
       control.value === matchingControl.value ? null : { noMatch: true };
+
+    matchingControl.setErrors(error);
 
     return error;
   };
